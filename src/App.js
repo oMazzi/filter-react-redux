1fa46375c5c2e07b734231cb0a16162718d8a8fe
Will <willian.maziero@hotmail.com>
2023-05-01 19:57:50 -0400

    done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Filter from './Filter';
+import Products from './Products';
+
+function App() {
+  return (
+    <div>
+      <Filter />
+      <Products />
+    </div>
+  );
+}
+
+export default App;
